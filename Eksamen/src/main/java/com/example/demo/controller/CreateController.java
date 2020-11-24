2cd429b83e84d999cb066397a6a12aa4bd255741
Blyatman666 <43138836+Blyatman666@users.noreply.github.com>
2020-11-24 19:15:08 +0100

    projekt i controlleren

--- Eksamen/src/main/java/com/example/demo/controller/CreateController.java
+++ Eksamen/src/main/java/com/example/demo/controller/CreateController.java
@@ -1,22 +1,43 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @Controller
 public class CreateController {
 
-    @GetMapping("createProject")
+    Project project = new Project();
+
+    @GetMapping("/createProject")
     public String createProject() {
         return "createProject";
     }
 
-    @PostMapping("createProjectForm")
-    public String createProject(
-            @
-    )
+    @PostMapping("/addEmployees")
+    public String addEmployees(){
+        return "addEmployees";
+    }
+
+    @PostMapping("/createProjectForm")
+    public String createProjectForm(
+            @ModelAttribute Project project,
+            @RequestParam int id,
+            @RequestParam String projectName,
+            @RequestParam Date deadline,
+            @RequestParam String description,
+            @RequestParam short numberOfEmployees
+    ){
+        System.out.println("Det virker!");
+        Project project1 = new Project(id, projectName, deadline, description, numberOfEmployees);
+        return "addEmployees";
+    }
 
 }
