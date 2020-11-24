b81a40a75c719866c24dfb56c72a05a922dc08cf
ande8548@stud.kea.dk <anders_h91@hotmail.com>
2020-11-24 14:00:49 +0100

    Login

--- Eksamen/src/main/java/com/example/demo/controller/myController.java
+++ Eksamen/src/main/java/com/example/demo/controller/myController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class myController {
@@ -10,4 +12,19 @@ public class myController {
     public String index(){
         return "index";
     }
+}
+
+
+    @PostMapping("/login")
+    public String loginUser(WebRequest request) throws LoginSampleException {
+        //Retrieve values from HTML form via WebRequest
+        String email = request.getParameter("email");
+        String pwd = request.getParameter("password");
+
+        // delegate work + data to login controller
+        User user = loginController.login(email, pwd);
+        setSessionInfo(request, user);
+
+        // Go to to page dependent on role
+        return "userpages/";
 }
