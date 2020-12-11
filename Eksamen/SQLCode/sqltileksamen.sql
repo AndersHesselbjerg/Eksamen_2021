16a33a1bf82ce4c484e30961709e014476eb4715
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2020-12-11 14:25:07 +0100

    ændret i sqlKode2

--- Eksamen/SQLCode/sqltileksamen.sql
+++ Eksamen/SQLCode/sqltileksamen.sql
@@ -2,7 +2,7 @@
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
 -- Schema mydb
@@ -18,26 +18,6 @@ CREATE SCHEMA IF NOT EXISTS `projektoplysninger` DEFAULT CHARACTER SET utf8mb4 C
 USE `projektoplysninger` ;
 
 -- -----------------------------------------------------
--- Table `projektoplysninger`.`subprojects`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `projektoplysninger`.`subprojects` (
-                                                                  `id` INT NOT NULL AUTO_INCREMENT,
-                                                                  `name` VARCHAR(45) NULL DEFAULT NULL,
-                                                                  `description` VARCHAR(16000) NULL DEFAULT NULL,
-                                                                  `projectID` INT NOT NULL,
-                                                                  PRIMARY KEY (`id`),
-                                                                  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-                                                                  INDEX `projectID_idx` (`projectID` ASC) VISIBLE,
-                                                                  CONSTRAINT `projectID`
-                                                                      FOREIGN KEY (`projectID`)
-                                                                          REFERENCES `projektoplysninger`.`projects` (`id`))
-    ENGINE = InnoDB
-    AUTO_INCREMENT = 5
-    DEFAULT CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `projektoplysninger`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `projektoplysninger`.`user` (
@@ -45,10 +25,9 @@ CREATE TABLE IF NOT EXISTS `projektoplysninger`.`user` (
                                                            `mail` VARCHAR(45) NOT NULL,
                                                            `password` VARCHAR(45) NOT NULL,
                                                            `isAdmin` INT NOT NULL DEFAULT '0',
-                                                           `adminID` INT NULL DEFAULT NULL,
                                                            PRIMARY KEY (`id`))
     ENGINE = InnoDB
-    AUTO_INCREMENT = 8
+    AUTO_INCREMENT = 7
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
@@ -61,25 +40,37 @@ CREATE TABLE IF NOT EXISTS `projektoplysninger`.`projects` (
                                                                `name` VARCHAR(45) NULL DEFAULT NULL,
                                                                `description` VARCHAR(16000) NULL DEFAULT NULL,
                                                                `numberOfEmployees` INT NULL DEFAULT NULL,
-                                                               `saved` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+                                                               `DeadlineTime` TIME NULL DEFAULT NULL,
                                                                `deadline` DATE NULL DEFAULT NULL,
                                                                `userID` INT NULL DEFAULT NULL,
-                                                               `subID` INT NULL DEFAULT NULL,
-                                                               `subName` VARCHAR(100) NULL DEFAULT NULL,
-                                                               `subDescription` VARCHAR(45) NULL DEFAULT NULL,
-                                                               `subProjectID` INT NULL DEFAULT NULL,
                                                                PRIMARY KEY (`id`),
-                                                               UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-                                                               INDEX `user_idx` (`userID` ASC) VISIBLE,
-                                                               INDEX `subProject_idx` (`subID` ASC) VISIBLE,
-                                                               CONSTRAINT `subProject`
-                                                                   FOREIGN KEY (`subID`)
-                                                                       REFERENCES `projektoplysninger`.`subprojects` (`id`),
+                                                               UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+                                                               INDEX `user_idx` (`userID` ASC),
                                                                CONSTRAINT `user`
                                                                    FOREIGN KEY (`userID`)
                                                                        REFERENCES `projektoplysninger`.`user` (`id`))
     ENGINE = InnoDB
-    AUTO_INCREMENT = 9
+    AUTO_INCREMENT = 4
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `projektoplysninger`.`subprojects`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `projektoplysninger`.`subprojects` (
+                                                                  `subId` INT NOT NULL AUTO_INCREMENT,
+                                                                  `subName` VARCHAR(45) NULL DEFAULT NULL,
+                                                                  `subDescription` VARCHAR(16000) NULL DEFAULT NULL,
+                                                                  `projectID` INT NOT NULL,
+                                                                  PRIMARY KEY (`subId`),
+                                                                  UNIQUE INDEX `id_UNIQUE` (`subId` ASC),
+                                                                  INDEX `projectID_idx` (`projectID` ASC),
+                                                                  CONSTRAINT `projectID`
+                                                                      FOREIGN KEY (`projectID`)
+                                                                          REFERENCES `projektoplysninger`.`projects` (`id`))
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 6
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
